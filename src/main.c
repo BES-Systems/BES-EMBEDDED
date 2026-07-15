33b178817df5d2481224c06a4e55c726fa9d0ccc
allexanderbergmns <socials@libregrad.pp.ua>
2026-07-15 20:13:53 +0200

    * Set test lights

--- src/main.c
+++ src/main.c
@@ -12,13 +12,17 @@
 
 #include <stdio.h>
 
+#define LED_PIN 4
+
+
 void app_main(void)
 {
     bes_boot_start();
 
-    bes_display_on();
-    bes_gpio_write(21, 1);
-    bes_display_fill(BES_RED);
+    // bes_display_on();
+    // bes_gpio_write(21, 1);
+    // bes_display_fill(BES_RED);
+    bes_gpio_output(LED_PIN);
 
     bes_task_create(
         "heartbeat",
@@ -30,8 +34,11 @@ void app_main(void)
 
     while(1)
     {
-        bes_task_delay(1000);
+        bes_gpio_write(LED_PIN, 1);
+        bes_delay(1000);
 
+        bes_gpio_write(LED_PIN, 0);
+        bes_delay(1000);
         // bes_display_on();
         // bes_delay(1000);
         // bes_display_off();
