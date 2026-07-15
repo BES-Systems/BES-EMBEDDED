c2d74bb802feec145d360459b1c2eac84abc984a
allexanderbergmns <socials@libregrad.pp.ua>
2026-07-15 18:13:37 +0200

    * BES color

---
+++ include/bes/color.h
@@ -0,0 +1,11 @@
+#ifndef BES_COLOR_H
+#define BES_COLOR_H
+
+#define BES_BLACK 0x0000
+#define BES_WHITE 0xFFFF
+
+#define BES_RED   0xF800
+#define BES_GREEN 0x07E0
+#define BES_BLUE  0x001F
+
+#endif
